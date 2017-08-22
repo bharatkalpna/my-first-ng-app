33e05ba348e4c56b3c136797227cef5e7c4c2119
bharatkalpna <bharatram108@gmail.com>
2017-08-23 01:48:40 +0530

    double chutia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {formsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,7 +9,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModel
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
